1796689e79d524a05745191bc5b278adf7fedec8
Zach Z <z.zukowski@gmail.com>
2020-12-03 17:33:33 -0500

    Some Stuff added

--- UI/hppnui/src/components/HomePage.js
+++ UI/hppnui/src/components/HomePage.js
@@ -1,9 +1,60 @@
+import React, { useState, useEffect } from "react";
+import api from "../services/api";
+
+import ListGroup from "react-bootstrap/ListGroup";
+import Container from "react-bootstrap/Container";
+
+
 const HomePage = () => {
+
+    const [events, setEvents] = useState(null);
+    const [events2, setEvents2] = useState(null);
+    const getEvents = async () => {
+        const response = await api.service("events").find({query: { id: 1 } });
+        setEvents(response.data);
+      };
+      const getEvents2 = async () => {
+        const response = await api.service("events").find({query: { id: 2 } });
+        setEvents2(response.data);
+      };
+
+      useEffect(() => {
+        getEvents();
+        getEvents2();
+        // getUsers();
+      }, []);
+
     return (
-       <div>
-          <h1>Home Page</h1>
-           <p>Home page body content</p>
-       </div>
+       <Container>
+           <h2>Your Events</h2>
+<ListGroup>
+{events &&
+            events.map((event, index) => {
+              return (
+                <ListGroup.Item>
+                 
+                    {event.name}
+                  
+                  </ListGroup.Item>
+              );
+            })}
+
+</ListGroup>
+<h2>Local Events</h2>
+<ListGroup>
+{events2 &&
+            events2.map((event, index) => {
+              return (
+                <ListGroup.Item>
+                 
+                    {event.name}
+                  
+                  </ListGroup.Item>
+              );
+            })}
+</ListGroup>
+
+       </Container>
     );
 }
  
--- UI/hppnui/src/components/MapPage.js
+++ UI/hppnui/src/components/MapPage.js
@@ -1,91 +1,88 @@
-import React, { useState, useEffect } from 'react';
-import api from '../services/api';
+import React, { useState, useEffect } from "react";
+import api from "../services/api";
 
-import ReactMapGL, {Marker} from 'react-map-gl';
-import Card from 'react-bootstrap/Card'
-import HppnnLogo from '../HppnnLogo';
+import Card from "react-bootstrap/Card";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import HppnnLogo from "../HppnnLogo";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const MapPage = () => {
-    const [users, setUsers] =  useState(null)
-    //const [events, setEvents] =  useState(null)
-    const [viewport, setViewport] = useState({
-        width: 400,
-        height: 400,
-        latitude: 38.4351,
-        longitude: -78.8698,
-        zoom: 11
-      });
-      const getUsers = async () => {
-        const response = await api.service('users').find({});
-        setUsers(response.data);
-      };
-    
-    //   const getEvents = async () => {
-    //     const response = await api.service('events').find({});
-    //     setEvents(response.data);
-    //   }; 
+  const [users, setUsers] = useState(null);
 
-      useEffect(() => {
-        //getEvents();
-        getUsers();
-        
-      }, []);
-    return(
-<div className="app">
-<link href="https://api.tiles.mapbox.com/mapbox-gl-js/v<1.12.0>/mapbox-gl.css" rel="stylesheet" />
-<h1>HPPNN Users</h1>
+  const getUsers = async (val) => {
+    const response = await api.service("users").find(
+      { 
+        query: 
+        {user_name: 
+            {
+              $eq: val
+            }
+        }
+      }
+      );
+    setUsers(response.data);
+  };
 
-<ReactMapGL id="map" mapboxApiAccessToken="pk.eyJ1Ijoienp1a293c2tpIiwiYSI6ImNraG8zeW12MTA2b24yeG1qYmJhb3czeXYifQ.r9bVmL1zpRLXkfOSHVgi8g"
-mapStyle="mapbox://styles/mapbox/outdoors-v11"
-{...viewport}
-onViewportChange={nextViewport => setViewport(nextViewport)}
->
-{users && users.map((user,index)=>{
-    return (
-      <Marker longitude={user.geo_location.coordinates[1]} latitude={user.geo_location.coordinates[0]}>
-  <div style={{color: "white",
-backgroundColor: "DodgerBlue",
-padding: ".5px"}}>{user.user_name}</div>
-  </Marker>
-    );
-  })}
+  useEffect(() => {
+    getUsers();
+  }, []);
 
-</ReactMapGL> 
-<div className="users">
-{users && users.map((user,index)=>{
-    const uname = user.user_name;
-   
-    
-    return (
-      
-<Card style={{ width: '18rem' }}>
-<Card.Header as="h5"><HppnnLogo className="App-logo"/>  {uname}</Card.Header>
-  <Card.Body>
-    
-    <Card.Subtitle className="mb-2 text-muted">{user.name}</Card.Subtitle>
-    <Card.Text>
-      {user.bio}
-    </Card.Text>
-    <Card.Link href="#">Add Friend</Card.Link>
-  </Card.Body>
-</Card>
+  function handleClick(e) {
+    let val = document.getElementById('uInput');
+    getUsers(val);
+    console.log('The link was clicked.');
+  }
+  return (
 
-        // <div className="user" key={index}>
-        //   <h2>{uname}</h2>
-        //     <h3>User {index+1}</h3>
-        //     <h3>{user.name}</h3>
-        //     <div className="details">
-        //       <p>bio: {user.bio}</p>
-        //       <p>Location: <br/> lat={user.geo_location.coordinates[0]} <br/> long={user.geo_location.coordinates[1]} </p>
-              
-        //     </div>
-        // </div>
-    );
-  })}
-</div>
-</div>
-)};
+    <Container className="users">
+      <InputGroup className="mb-3">
+    <InputGroup.Prepend>
+      <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+    </InputGroup.Prepend>
+    <FormControl 
+      id="uInput"
+      placeholder="Username"
+      aria-label="Username"
+      aria-describedby="basic-addon1"
+    />
+    <Button type="submit"  >Search</Button>
+  </InputGroup>
+
+      {users &&
+        users.map((user, index) => {
+          const uname = user.user_name;
+
+          return (
+            <Card style={{ width: "18rem" }}>
+              <Card.Header as="h5">
+                <HppnnLogo className="App-logo" /> {uname}
+              </Card.Header>
+              <Card.Body>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {user.name}
+                </Card.Subtitle>
+                <Card.Text>{user.bio}</Card.Text>
+                <Card.Link href="#">Add Friend</Card.Link>
+              </Card.Body>
+            </Card>
+
+            // <div className="user" key={index}>
+            //   <h2>{uname}</h2>
+            //     <h3>User {index+1}</h3>
+            //     <h3>{user.name}</h3>
+            //     <div className="details">
+            //       <p>bio: {user.bio}</p>
+            //       <p>Location: <br/> lat={user.geo_location.coordinates[0]} <br/> long={user.geo_location.coordinates[1]} </p>
+
+            //     </div>
+            // </div>
+          );
+        })}
+    </Container>
+  );
+};
 export default MapPage;
