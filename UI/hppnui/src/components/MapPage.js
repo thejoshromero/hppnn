d1190d59dff6478bde65ede5440975433058c80f
Zach Z <z.zukowski@gmail.com>
2020-12-04 18:16:03 -0500

    got search working

--- UI/hppnui/src/components/MapPage.js
+++ UI/hppnui/src/components/MapPage.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
 import api from "../services/api";
 
 import Card from "react-bootstrap/Card";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
+import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import HppnnLogo from "../HppnnLogo";
@@ -11,78 +12,109 @@ import HppnnLogo from "../HppnnLogo";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const MapPage = () => {
-  const [users, setUsers] = useState(null);
-
-  const getUsers = async (val) => {
-    const response = await api.service("users").find(
-      { 
-        query: 
-        {user_name: 
-            {
-              $eq: val
-            }
-        }
-      }
-      );
-    setUsers(response.data);
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [users, setUsers] =  useState(null)
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
   };
 
-  useEffect(() => {
-    getUsers();
-  }, []);
+  const results = !searchTerm
+    ? users
+    : users.filter(user =>
+        user.user_name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+      );
 
-  function handleClick(e) {
-    let val = document.getElementById('uInput');
-    getUsers(val);
-    console.log('The link was clicked.');
-  }
-  return (
+    const getUsers = async () => {
+      const response = await api.service('users').find({});
+      setUsers(response.data);
+    };
+  
 
-    <Container className="users">
-      <InputGroup className="mb-3">
-    <InputGroup.Prepend>
-      <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-    </InputGroup.Prepend>
-    <FormControl 
-      id="uInput"
-      placeholder="Username"
-      aria-label="Username"
-      aria-describedby="basic-addon1"
-    />
-    <Button type="submit"  >Search</Button>
-  </InputGroup>
+    //   const getUsers = async () => {
+    //   const response = await api.service("users").find(
+    //     { 
+    //       // query: 
+    //       // {user_name: 
+    //       //     {
+    //       //       $like: `%${this.state.val}%`
+    //       //     }
+    //       // }
+    //     }
+    //     );
+    //   setUsers(response.data);
+      
+    // };
+  
+    useEffect(() => {
+      //getEvents();
+       getUsers();
+    }, []);
+  
 
-      {users &&
-        users.map((user, index) => {
-          const uname = user.user_name;
+    
+      return (
+        
+        <Container className="App">
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        
+          {results && results.map(item => (
+            <Card >
+                     <Card.Header as="h5">
+                       <HppnnLogo className="App-logo" /> {item.user_name}
+                     </Card.Header>
+                     <Card.Body>
+                       <Card.Subtitle className="mb-2 text-muted">
+                         {item.name}
+                       </Card.Subtitle>
+                       <Card.Text>{item.bio}</Card.Text>
+                       <Card.Link href="#">Add Friend</Card.Link>
+                     </Card.Body>
+                   </Card>
+          ))}
+      
+      </Container>
 
-          return (
-            <Card style={{ width: "18rem" }}>
-              <Card.Header as="h5">
-                <HppnnLogo className="App-logo" /> {uname}
-              </Card.Header>
-              <Card.Body>
-                <Card.Subtitle className="mb-2 text-muted">
-                  {user.name}
-                </Card.Subtitle>
-                <Card.Text>{user.bio}</Card.Text>
-                <Card.Link href="#">Add Friend</Card.Link>
-              </Card.Body>
-            </Card>
 
-            // <div className="user" key={index}>
-            //   <h2>{uname}</h2>
-            //     <h3>User {index+1}</h3>
-            //     <h3>{user.name}</h3>
-            //     <div className="details">
-            //       <p>bio: {user.bio}</p>
-            //       <p>Location: <br/> lat={user.geo_location.coordinates[0]} <br/> long={user.geo_location.coordinates[1]} </p>
 
-            //     </div>
-            // </div>
-          );
-        })}
-    </Container>
-  );
+        // {this.state.users &&
+        //   this.state.users.map((user) => {
+        //     const uname = user.user_name;
+  
+        //     return (
+        //       <Card style={{ width: "18rem" }}>
+        //         <Card.Header as="h5">
+        //           <HppnnLogo className="App-logo" /> {uname}
+        //         </Card.Header>
+        //         <Card.Body>
+        //           <Card.Subtitle className="mb-2 text-muted">
+        //             {user.name}
+        //           </Card.Subtitle>
+        //           <Card.Text>{user.bio}</Card.Text>
+        //           <Card.Link href="#">Add Friend</Card.Link>
+        //         </Card.Body>
+        //       </Card>
+        //     );
+        //   })}
+        
+      
+
+
+        
+      );
+    
+  
+  // const [name, setName] = useState("");
+  
+  // const handleSubmit = (evt) => {
+  //     evt.preventDefault();
+  //     alert(`Submitting Name ${name}`)
+  // }
+  // const [users, setUsers] = useState(null);
+
 };
 export default MapPage;
