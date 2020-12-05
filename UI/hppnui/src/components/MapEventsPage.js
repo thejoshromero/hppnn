0e30efe4e5e507af4c71af162b4e3feb97dd6572
Zach Z <z.zukowski@gmail.com>
2020-12-04 19:01:09 -0500

    stuffs

--- UI/hppnui/src/components/MapEventsPage.js
+++ UI/hppnui/src/components/MapEventsPage.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from "react";
 import Carousel from "react-bootstrap/Carousel";
+import Toast from "react-bootstrap/Toast";
+import * as Icon from "react-bootstrap-icons";
 import Container from "react-bootstrap/Container";
 import api from "../services/api";
+import HppnnLogoBig from "../HppnnLogoBig";
+import HppnnLogo from "../HppnnLogo";
 
 import ReactMapGL, { Marker } from "react-map-gl";
 
 
+
 const MapEventsPage = () => {
-  //const [users, setUsers] =  useState(null)
   const [events, setEvents] = useState(null);
+  const [showA, setShowA] = useState(true);
   const [viewport, setViewport] = useState({
     width: 400,
     height: 400,
@@ -16,11 +21,7 @@ const MapEventsPage = () => {
     longitude: -78.8698,
     zoom: 11,
   });
-  //   const getUsers = async () => {
-  //     const response = await api.service('users').find({});
-  //     setUsers(response.data);
-  //   };
-
+  const toggleShowA = () => setShowA(!showA);
   const getEvents = async () => {
     const response = await api.service("events").find({});
     setEvents(response.data);
@@ -28,16 +29,16 @@ const MapEventsPage = () => {
 
   useEffect(() => {
     getEvents();
-    // getUsers();
   }, []);
   return (
     <Container justify>
       <div>
+      <HppnnLogoBig className="App-logo-title"></HppnnLogoBig>
         <link
           href="https://api.tiles.mapbox.com/mapbox-gl-js/v<1.12.0>/mapbox-gl.css"
           rel="stylesheet"
         />
-        <h1>HPPNN Events</h1>
+        
 
         <ReactMapGL
           id="map"
@@ -53,15 +54,26 @@ const MapEventsPage = () => {
                   longitude={event.geo_location.coordinates[1]}
                   latitude={event.geo_location.coordinates[0]}
                 >
-                  <div
+                  <Icon.GeoAlt />
+                  <Toast show={showA} onClose={toggleShowA}>
+          <Toast.Header>
+            <HppnnLogo className="App-logo"/>
+            <strong className="mr-auto">{event.name}</strong>
+            <small>{event.start_time}</small>
+          </Toast.Header>
+          <Toast.Body>{event.description}</Toast.Body>
+        </Toast>
+                  {/* <div
                     style={{
                       color: "white",
-                      backgroundColor: "DodgerBlue",
+                      backgroundColor: "Gray",
                       padding: ".5px",
                     }}
                   > 
                     {event.name}
-                  </div>
+                    <div>{event.description}</div>
+                  </div> */}
+                 
                 </Marker>
               );
             })}
@@ -72,7 +84,7 @@ const MapEventsPage = () => {
                       color: "white",
                       backgroundColor: "Gray",
                       padding: ".5px",
-                      width: "50%",
+                      width: "100%",
                     }}>
         
         {events &&
@@ -83,8 +95,8 @@ const MapEventsPage = () => {
                   <h3 style={{'color':"blue"}} >{event.name}</h3>
                   <p style={{'color':"black"}}>{event.description}</p>
                   <p style={{'color':"black"}}>{event.city}</p>
-                  <p style={{'color':"black"}}>{event.state}</p>
-                  <p style={{'color':"black"}}>{event.zip_code}</p>
+                  {/* <p style={{'color':"black"}}>{event.state}</p> */}
+                  {/* <p style={{'color':"black"}}>{event.zip_code}</p> */}
                   <p style={{'color':"black"}}>{event.start_time}</p>
                 </Carousel.Caption>
               </Carousel.Item>
