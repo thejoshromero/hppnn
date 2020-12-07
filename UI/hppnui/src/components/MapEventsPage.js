77a00a75a2866783454cdbf8c2eaa272ff1cb952
Zach Z <z.zukowski@gmail.com>
2020-12-07 17:21:44 -0500

    toasts closing

--- UI/hppnui/src/components/MapEventsPage.js
+++ UI/hppnui/src/components/MapEventsPage.js
@@ -10,13 +10,16 @@ import HppnnLogo from "../HppnnLogo";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Moment from "react-moment";
+
+import * as moment from "react-moment";
 
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
 
 const MapEventsPage = () => {
-  const [events, setEvents] = useState(null);
+  const [events, setEvents] = useState([]);
   const [showPopup, setShowPopup] = useState(null);
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState([]);
   const [showA, setShowA] = useState(true);
   const [viewport, setViewport] = useState({
     width: "100%",
@@ -25,7 +28,7 @@ const MapEventsPage = () => {
     longitude: -78.8698,
     zoom: 11,
   });
-  const toggleShowA = () => setShowA(!showA);
+
   const getEvents = async () => {
     const response = await api.service("events").find({});
     setEvents(response.data);
@@ -61,16 +64,20 @@ const MapEventsPage = () => {
   useEffect(() => {
     getEvents();
   }, []);
+
+  useEffect(() => {
+    setShow(events.map(() => true));
+  }, [events]);
   return (
     <Container style={{ height: "95%" }}>
       <div>
-      <Row>
-           <Col/>
-           <Col>
-        <HppnnLogoBig className="App-logo-title"></HppnnLogoBig>
-        </Col>
-           <Col/>
-           </Row>
+        <Row>
+          <Col />
+          <Col>
+            <HppnnLogoBig className="App-logo-title"></HppnnLogoBig>
+          </Col>
+          <Col />
+        </Row>
         <link
           href="https://api.tiles.mapbox.com/mapbox-gl-js/v<1.12.0>/mapbox-gl.css"
           rel="stylesheet"
@@ -85,8 +92,13 @@ const MapEventsPage = () => {
         >
           {events &&
             events.map((event, index) => {
+              //const stTime = moment(event.start_time);
+              const dur = event.duration;
+              //let endTime = new Moment(stTime, 'HH:mm:ss');//.add(dur, 'hours');
+              //var endTime2 = new Moment(stTime, 'DD-MM-YYYY').add('month', 1);
               return (
                 <div>
+                  <Icon.GeoAlt />
                   <Marker
                     longitude={event.geo_location.coordinates[1]}
                     latitude={event.geo_location.coordinates[0]}
@@ -96,11 +108,29 @@ const MapEventsPage = () => {
                   >
                     <Icon.GeoAlt />
 
-                    <Toast>
+                    <Toast
+                      onClose={() => {
+                        const newShow = [...show];
+                        newShow[index] = false;
+                        setShow(newShow);
+                      }}
+                      show={show[index]}
+                    >
                       <Toast.Header>
                         <HppnnLogo className="App-logo-marker" />
                         <strong className="mr-auto">{event.name}</strong>
-                        <small>{event.start_time}</small>
+                        <small>
+                          <Moment format="MMM DD" date={event.start_time} />
+                          &nbsp;{" "}
+                        </small>
+                        <small>
+                          <Moment format="hh:mm" date={event.start_time} /> to{" "}
+                          <Moment
+                            format="hh:mm"
+                            date={event.start_time}
+                            add={{ hours: 3 }}
+                          />{" "}
+                        </small>
                       </Toast.Header>
                       <Toast.Body>{event.description}</Toast.Body>
                     </Toast>
@@ -140,7 +170,7 @@ const MapEventsPage = () => {
                     }}
                     align="center"
                   >
-                    <h5 style={{ color: "blue" }}>
+                    <h5 style={{ color: "blue", "font-family": "Courier New" }}>
                       <HppnnLogo className="App-logo-marker" /> {event.name}
                     </h5>
                     <div>
@@ -156,7 +186,9 @@ const MapEventsPage = () => {
             );
           })}
       </Carousel>
-      <Button onClick={() => alert("need to implement")} alignment="center">Add Event</Button>
+      <Button onClick={() => alert("need to implement")} alignment="center">
+        Add Event
+      </Button>
     </Container>
   );
 };
